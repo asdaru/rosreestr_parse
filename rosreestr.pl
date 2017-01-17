a50ea5f4a43129e9927c55848364ee918debf0fd
asda_house <asda@asda.ru>
2017-01-17 18:25:05 +0300

    new api

--- rosreestr.pl
+++ rosreestr.pl
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env perl
 use lib '/home/asda/workspace/ruscience/mojo/lib';
 use Mojo::Base 'Mojolicious::Controller';
 use Mojo::UserAgent;
@@ -11,17 +11,15 @@ use Mojo::URL;
 my $ua = Mojo::UserAgent->new;
 
 foreach ( 1 .. 1500 ) {
-	my $res = $ua->get( 'http://maps.rosreestr.ru/arcgis/rest/services/Cadastre/CadastreSelected/MapServer/exts/GKNServiceExtension/online/parcel/find?cadNum=50:04:0180301:' . $_ . '&onlyAttributes=false&f=json' => { DNT => 1 } )->res;
-	my $dom=$res->json;
-#	my $dom = Mojo::DOM->new( $res->body );
-	my $CP_VALUE = $dom->{features}->[0]->{attributes}->{CP_VALUE};
-	
-	print '50:04:0180301:' . $_ ."\t".$CP_VALUE*1000."\n" if ($CP_VALUE)
-	#warn $CP_VALUE->{value};
-	#use Data::Dumper; warn Dumper($CP_VALUE);
-	#warn $_;
+	my $res = $ua->get( 'https://pkk5.rosreestr.ru/api/features/1/50:4:180301:' . $_ => { DNT => 1 } )->res;
+	my $dom = $res->json;
 
-	#use Data::Dumper;warn Dumper( $dom->{features}->[0]->{attributes}->{CP_VALUE} );
+	next unless $dom->{feature};
+
+	my $attrs = $dom->{feature}->{attrs};
+
+	print $attrs->{id}. "\t" . $attrs->{cad_cost}. "\n" ;
+	#print $attrs->{id}. "\t" . $attrs->{cad_cost}."\t".($attrs->{cad_eng_data} ? $attrs->{cad_eng_data}->{co_name}:""). "\n" ;
 
 }
 
